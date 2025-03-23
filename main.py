4aefc20318221b7f8d76a9a82182191285503f8d
theworksofvon <davontaejackson667@gmail.com>
2025-03-23 17:00:24 -0500

    remove hot reload

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ async def shutdown():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
